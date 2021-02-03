c8fe93e6fc1ea8f5fc705a27659bfe0fb228a928
zhang <zhangfutian1@gmail.com>
2021-02-02 21:36:37 -0800

    first

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,39 @@
 // main.js
 
 // TODO
+
+let volumn = 100;
+let textfield = document.getElementById("volume-number");
+let slider = document.getElementById("volume-slider");
+let volumn_image = document.getElementById("volume-image");
+
+textfield.onchange = function(){
+    volumn = textfield.nodeValue;
+    slider.nodeValue = volumn;
+    if (volumn >= 67 && volumn <= 100){
+        volumn_image.src = "./assets/media/icons/volume-level-3.svg";
+    }else if (volumn >= 34){
+        volumn_image.src = "./assets/media/icons/volume-level-2.svg";
+    }else if (volumn >= 1){
+        volumn_image.src = "./assets/media/icons/volume-level-1.svg";
+    }else{
+        volumn_image.src = "./assets/media/icons/volume-level-0.svg";
+    }
+};
+
+slider.onchange = function(){
+    volumn = slider.nodeValue;
+    textfield.nodeValue = volumn;
+    if (volumn >= 67 && volumn <= 100){
+        volumn_image.src = "./assets/media/icons/volume-level-3.svg";
+    }else if (volumn >= 34){
+        volumn_image.src = "./assets/media/icons/volume-level-2.svg";
+    }else if (volumn >= 1){
+        volumn_image.src = "./assets/media/icons/volume-level-1.svg";
+    }else{
+        volumn_image.src = "./assets/media/icons/volume-level-0.svg";
+    }
+};
+
+let image = document.getElementById("sound-image");
+
